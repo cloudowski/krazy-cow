f60d2bb7b01d619e98eb3fabdfe5cd0b087b01ad
Tomasz Cholewa <tomasz@cloudowski.com>
2019-02-21 22:24:55 +0000

    Added feeding feature for cow

--- pkg/cow/cow.go
+++ pkg/cow/cow.go
@@ -2,9 +2,12 @@ package cow
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"regexp"
 	"time"
 )
 
@@ -69,7 +72,7 @@ func (c *Cow) SetFree(w http.ResponseWriter, r *http.Request) {
 // Returns ok only if cow is happy (see happyThreshold)
 func (c *Cow) Healthcheck(w http.ResponseWriter, r *http.Request) {
 	if c.GetMood() >= happyThreshold {
-		fmt.Fprint(w, "MooOK")
+		fmt.Fprintln(w, "MooOK")
 	} else {
 		http.Error(w, fmt.Sprintf("I am not ok, mood level: %v", c.GetMood()), http.StatusBadRequest)
 	}
@@ -85,5 +88,67 @@ func (c *Cow) MoodChanger(intervalSeconds int, moodChange int) {
 			c.SetMood(c.GetMood() + moodChange)
 		}
 		log.Println("MoodChanger - current mood:", c.GetMood())
+	}
+}
+
+// eat tuft from pasture id available
+func (c *Cow) Grass(path string, interval int) {
+	if f, err := os.Stat(path); err != nil {
+		log.Println("Failed to check pasture:", err)
+		return
+
+	} else if f.IsDir() {
+		log.Printf("Going to eat from %v, interval: %vs\n", path, interval)
+	} else {
+		log.Println(err)
+		return
+	}
+
+	tuftGreen := regexp.MustCompile("^tuft")
+	tuftEaten := regexp.MustCompile("\\.eaten_by")
+
+	var ate bool
+
+	for {
+		time.Sleep(time.Duration(interval) * time.Second)
+
+		ate = false
+
+		files, err := ioutil.ReadDir(path)
+
+		if err != nil {
+			log.Println("Could not find tuft:", err)
+			continue
+		}
+
+		for _, f := range files {
+			if f.IsDir() {
+				continue
+			}
+			tuft := f.Name()
+			if tuftEaten.MatchString(tuft) {
+
+				continue
+			} else if tuftGreen.MatchString(tuft) {
+				log.Println("Eating tuft:", tuft)
+				if err := os.Rename(filepath.Join(path, tuft), filepath.Join(path, fmt.Sprintf(".%v.eaten_by_%v", tuft, c.Name))); err != nil {
+					log.Fatalln("Could not eat tuft:", err)
+				}
+				ate = true
+				break
+
+			} else {
+				log.Println("NOT a tuft:", tuft)
+			}
+
+		}
+
+		if ate {
+			c.SetMood(c.GetMood() + 2)
+			log.Println("Happiness increased - mood:", c.GetMood())
+		} else if c.GetMood() > 0 {
+			c.SetMood(c.GetMood() - 1)
+			log.Println("No food - getting sad and angry - mood decreased:", c.GetMood())
+		}
 	}
 }
